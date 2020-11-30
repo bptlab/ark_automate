e0b7ca4046967415d2cfeb4bf4a54eed83435271
DanielWoelki <daniel.woelki@outlook.de>
2020-11-30 11:00:27 +0100

    Implement Button to get XML of BPMN Diagram

--- client/src/components/bpmn.modeler.component.jsx
+++ client/src/components/bpmn.modeler.component.jsx
@@ -1,69 +1,74 @@
-import React, {Component} from "react";
-import BpmnModeler from "bpmn-js/lib/Modeler";
-import "bpmn-js/dist/assets/diagram-js.css";
-import "bpmn-font/dist/css/bpmn-embedded.css";
-import {emptyBpmn} from "../assets/empty.bpmn";
-import propertiesPanelModule from "bpmn-js-properties-panel";
-import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
-import camundaModdleDescriptor from "camunda-bpmn-moddle/resources/camunda";
+import React, { Component } from 'react';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-font/dist/css/bpmn-embedded.css';
+import { emptyBpmn } from '../assets/empty.bpmn';
+import propertiesPanelModule from 'bpmn-js-properties-panel';
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
 
 class BpmnModelerComponent extends Component {
-    modeler = null;
+  modeler = null;
 
-    componentDidMount = () => {
-        this.modeler = new BpmnModeler({
-            container: "#bpmnview",
-            keyboard: {
-                bindTo: window,
-            },
-            propertiesPanel: {
-                parent: "#propview",
-            },
-            additionalModules: [propertiesPanelModule, propertiesProviderModule],
-            moddleExtensions: {
-                camunda: camundaModdleDescriptor,
-            },
-        });
+  componentDidMount = () => {
+    this.modeler = new BpmnModeler({
+      container: '#bpmnview',
+      keyboard: {
+        bindTo: window,
+      },
+      propertiesPanel: {
+        parent: '#propview',
+      },
+      additionalModules: [propertiesPanelModule, propertiesProviderModule],
+      moddleExtensions: {
+        camunda: camundaModdleDescriptor,
+      },
+    });
 
-        this.newBpmnDiagram();
-    };
+    this.newBpmnDiagram();
+  };
 
-    newBpmnDiagram = () => {
-        this.openBpmnDiagram(emptyBpmn);
-    };
+  newBpmnDiagram = () => {
+    this.openBpmnDiagram(emptyBpmn);
+  };
 
-    openBpmnDiagram = (xml) => {
-        this.modeler.importXML(xml, (error) => {
-            if (error) {
-                return console.log("fail import xml");
-            }
+  openBpmnDiagram = (xml) => {
+    this.modeler.importXML(xml, (error) => {
+      if (error) {
+        return console.log('fail import xml');
+      }
 
-            var canvas = this.modeler.get("canvas");
+      var canvas = this.modeler.get('canvas');
 
-            canvas.zoom("fit-viewport");
-        });
-    };
+      canvas.zoom('fit-viewport');
+    });
+  };
+  getBpmnDiagramXML = () => {
+    this.modeler.saveXML().then((xml) => console.log(xml));
+  };
 
-    render = () => {
-        return (
-            <div id="bpmncontainer">
-                <div
-                    id="propview"
-                    style={{
-                        width: "25%",
-                        height: "98vh",
-                        float: "right",
-                        maxHeight: "98vh",
-                        overflowX: "auto",
-                    }}
-                ></div>
-                <div
-                    id="bpmnview"
-                    style={{width: "75%", height: "98vh", float: "left"}}
-                ></div>
-            </div>
-        );
-    };
+  render = () => {
+    return (
+      <div id='bpmncontainer'>
+        <div
+          id='propview'
+          style={{
+            width: '25%',
+            height: '98vh',
+            float: 'right',
+            maxHeight: '98vh',
+            overflowX: 'auto',
+          }}
+        ></div>
+        <div
+          id='bpmnview'
+          style={{ width: '75%', height: '98vh', float: 'left' }}
+        >
+          <button onClick={this.getBpmnDiagramXML}>XMLButton</button>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default BpmnModelerComponent;
