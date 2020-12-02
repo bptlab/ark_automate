b6f5fb0c3debc0f9607bf33bbce62676922e91a0
Sandro Speh <sandro.speh@student.hpi.de>
2020-12-02 15:18:02 +0100

    Working javascript parser in backend implemented but with values passed in extensions tab in modeler

--- server/parsing/parser.js
+++ server/parsing/parser.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const { parse } = require('path');
+const fs = require('fs')
+const { parse } = require('path')
 
 /**
  * @description Parses an JSON created from the .bpmn xml of the model to an executable .robot file
@@ -7,81 +7,73 @@ const { parse } = require('path');
  */
 
 // this should be refactored to a dictionary
-function taskLib(app) {
-  if (app == 'Excel') return 'RPA.Excel.Files';
-  if (app == 'Browser') return 'RPA.Browser';
+function taskLib (app) {
+  if (app == 'Excel') return 'RPA.Excel.Files'
+  if (app == 'Browser') return 'RPA.Browser'
 }
-//var json_data = require('./convertedModel.json');
 
-function parseXML(json_data) {
-  console.log(json_data);
-
-  var parsedCode = '';
-
-  var bpmnTasks = json_data['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
-  console.log(bpmnTasks['_attributes']['name']);
+function parseXML (json_data) {
+  let parsedCode = ''
+  const bpmnTasks = json_data['bpmn2:definitions']['bpmn2:process']['bpmn2:task']
 
   // create Settings statements
-  parsedCode += '*** Settings ***\n';
-  parsedCode += 'Documentation  Our first parsed RPA\n';
+  parsedCode += '*** Settings ***\n'
+  parsedCode += 'Documentation  Our first parsed RPA\n'
 
   // collect apps being used
-  let applications = [];
+  const applications = []
   for (task in bpmnTasks) {
-    let rpaApp =
-      bpmnTasks[task].extensionElements.properties.property[0]._value;
+    const rpaApp =
+      bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties']['camunda:property'][0]._attributes.value
     if (!applications.includes(rpaApp)) {
-      applications.push(rpaApp);
+      applications.push(rpaApp)
     }
   }
-  console.log(applications);
 
   // import libraries needed for apps
   for (app in applications) {
-    parsedCode += 'Library    ' + taskLib(applications[app]) + '\n';
+    parsedCode += 'Library    ' + taskLib(applications[app]) + '\n'
   }
 
-  parsedCode += '\n*** Tasks ***\n';
-  var codeToAppend = '';
+  // idealy we use the keyword statement for each task, currently not working out of the box
+  parsedCode += '\n*** Tasks ***\n'
 
   // add code for each task
-  for (task in bpmnTasks) {
-    // idealy we use the keyword statement for each task, currently not working out of the box
 
-    codeToAppend += bpmnTasks[task]._name + '\n';
-    let rpaTask, rpaTaskProps;
-    rpaTaskProps = bpmnTasks[task].extensionElements.properties.property;
-    rpaTask = rpaTaskProps[1]._value;
+  let codeToAppend = ''
+  for (task in bpmnTasks) {
+    codeToAppend += bpmnTasks[task]._attributes.name + '\n'
+    let rpaTask, rpaTaskProps
+    rpaTaskProps = bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties']['camunda:property']
+    const rpaTask = rpaTaskProps[1]._attributes.value
 
     // this should be refactored to a dictonary
     if (rpaTask == 'Open Browser')
-      codeToAppend += '   Open Browser    ' + rpaTaskProps[2]['_value'] + '\n';
+    // url of website to visit
+    { codeToAppend += '   Open Browser    ' + rpaTaskProps[2]._attributes.value + '\n' }
     if (rpaTask == 'Open Workbook')
-      codeToAppend += '   Open Workbook   ' + rpaTaskProps[2]['_value'] + '\n';
+    // path to workbook
+    { codeToAppend += '   Open Workbook   ' + rpaTaskProps[2]._attributes.value + '\n' }
     if (rpaTask == 'Set Worksheet Value') {
       codeToAppend +=
         '   Set Worksheet Value  ' +
-        rpaTaskProps[2]['_value'] +
+        // row
+        rpaTaskProps[2]._attributes.value +
         '  ' +
-        rpaTaskProps[3]['_value'] +
+        // column
+        rpaTaskProps[3]._attributes.value +
         '  ' +
-        rpaTaskProps[4]['_value'] +
-        '\n';
-      codeToAppend += '   Save Workbook\n';
+        // value
+        rpaTaskProps[4]._attributes.value +
+        '\n'
+      codeToAppend += '   Save Workbook\n'
     }
   }
 
-  parsedCode += codeToAppend;
+  parsedCode += codeToAppend
 
-  fs.writeFile('./JSrobot.robot', parsedCode, () => {});
-  console.log(parsedCode);
+  fs.writeFile('./JSrobot.robot', parsedCode, () => {})
+  console.log(parsedCode)
 }
 
-module.exports = { parseXML };
-/* var obj;
-fs.readFile('./convertedModel.json', 'utf8', function (err, data) {
-    obj = JSON.parse(data);
-    var test = obj[1];
-    console.log(test);
-   });
- */
+module.exports = { parseXML }
