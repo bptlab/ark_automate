d8df9771a24129a3e7414c524b0395f517b414b9
DanielWoelki <daniel.woelki@outlook.de>
2020-12-07 13:42:14 +0100

    Started refactoring of parser

--- client/src/parser.js
+++ client/src/parser.js
@@ -13,73 +13,113 @@ function taskLib(app) {
 
 function parseDiagramJson(json_data) {
   let parsedCode = '';
-  const bpmnTasks =
-    json_data['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
+  var bpmnTasks = json_data['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
 
   // create Settings statements
   parsedCode += '*** Settings ***\n';
   parsedCode += 'Documentation  Our first parsed RPA\n';
 
   // collect apps being used
-  const applications = [];
-  for (task in bpmnTasks) {
-    const rpaApp =
-      bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
-        'camunda:property'
-      ][0]._attributes.value;
-    if (!applications.includes(rpaApp)) {
-      applications.push(rpaApp);
+
+  if (bpmnTasks !== undefined) {
+    const applications = [];
+    for (let task in bpmnTasks) {
+      if (bpmnTasks[task]['bpmn2:extensionElements'] !== undefined) {
+        const rpaApp =
+          bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
+            'camunda:property'
+          ][0]._attributes.value;
+        if (!applications.includes(rpaApp)) {
+          applications.push(rpaApp);
+        }
+      }
     }
+
+    // import libraries needed for apps
+    for (let app in applications) {
+      parsedCode += 'Library    ' + taskLib(applications[app]) + '\n';
+    }
+
+    // idealy we use the keyword statement for each task, currently not working out of the box
+    parsedCode += '\n*** Tasks ***\n';
+
+    // add code for each task
+
+    let codeToAppend = '';
+    if (typeof bpmnTasks === 'object') {
+      let task = bpmnTasks;
+      codeToAppend += task['_attributes']['name'] + '\n';
+      let rpaTask, rpaTaskProps;
+      rpaTaskProps =
+        task['bpmn2:extensionElements']['camunda:properties'][
+          'camunda:property'
+        ];
+      rpaTask = rpaTaskProps[1]._attributes.value;
+
+      // this should be refactored to a dictonary
+      if (rpaTask == 'Open Browser') {
+        // url of website to visit
+        codeToAppend +=
+          '   Open Browser    ' + rpaTaskProps[2]._attributes.value + '\n';
+      }
+      if (rpaTask == 'Open Workbook') {
+        // path to workbook
+        codeToAppend +=
+          '   Open Workbook   ' + rpaTaskProps[2]._attributes.value + '\n';
+      }
+      if (rpaTask == 'Set Worksheet Value') {
+        codeToAppend +=
+          '   Set Worksheet Value  ' +
+          // row
+          rpaTaskProps[2]._attributes.value +
+          '  ' +
+          // column
+          rpaTaskProps[3]._attributes.value +
+          '  ' +
+          // value
+          rpaTaskProps[4]._attributes.value +
+          '\n';
+        codeToAppend += '   Save Workbook\n';
+      }
+    } else {
+      for (let task in bpmnTasks) {
+        codeToAppend += bpmnTasks[task]['_attributes']['name'] + '\n';
+        let rpaTask, rpaTaskProps;
+        rpaTaskProps =
+          bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
+            'camunda:property'
+          ];
+        rpaTask = rpaTaskProps[1]._attributes.value;
+
+        // this should be refactored to a dictonary
+        if (rpaTask == 'Open Browser') {
+          // url of website to visit
+          codeToAppend +=
+            '   Open Browser    ' + rpaTaskProps[2]._attributes.value + '\n';
+        }
+        if (rpaTask == 'Open Workbook') {
+          // path to workbook
+          codeToAppend +=
+            '   Open Workbook   ' + rpaTaskProps[2]._attributes.value + '\n';
+        }
+        if (rpaTask == 'Set Worksheet Value') {
+          codeToAppend +=
+            '   Set Worksheet Value  ' +
+            // row
+            rpaTaskProps[2]._attributes.value +
+            '  ' +
+            // column
+            rpaTaskProps[3]._attributes.value +
+            '  ' +
+            // value
+            rpaTaskProps[4]._attributes.value +
+            '\n';
+          codeToAppend += '   Save Workbook\n';
+        }
+      }
+    }
+    parsedCode += codeToAppend;
   }
-
-  // import libraries needed for apps
-  for (app in applications) {
-    parsedCode += 'Library    ' + taskLib(applications[app]) + '\n';
-  }
-
-  // idealy we use the keyword statement for each task, currently not working out of the box
-  parsedCode += '\n*** Tasks ***\n';
-
-  // add code for each task
-
-  let codeToAppend = '';
-  for (task in bpmnTasks) {
-    codeToAppend += bpmnTasks[task]._attributes.name + '\n';
-    let rpaTask, rpaTaskProps;
-    rpaTaskProps =
-      bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
-        'camunda:property'
-      ];
-    rpaTask = rpaTaskProps[1]._attributes.value;
-
-    // this should be refactored to a dictonary
-    if (rpaTask == 'Open Browser') {
-      // url of website to visit
-      codeToAppend +=
-        '   Open Browser    ' + rpaTaskProps[2]._attributes.value + '\n';
-    }
-    if (rpaTask == 'Open Workbook') {
-      // path to workbook
-      codeToAppend +=
-        '   Open Workbook   ' + rpaTaskProps[2]._attributes.value + '\n';
-    }
-    if (rpaTask == 'Set Worksheet Value') {
-      codeToAppend +=
-        '   Set Worksheet Value  ' +
-        // row
-        rpaTaskProps[2]._attributes.value +
-        '  ' +
-        // column
-        rpaTaskProps[3]._attributes.value +
-        '  ' +
-        // value
-        rpaTaskProps[4]._attributes.value +
-        '\n';
-      codeToAppend += '   Save Workbook\n';
-    }
-  }
-
-  parsedCode += codeToAppend;
   console.log(parsedCode);
   return parsedCode;
 }
