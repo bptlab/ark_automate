f5feceff11636bbc83338e28ec3760f5ae1ee18c
Sandro Speh <sandro.speh@student.hpi.de>
2020-12-09 11:14:13 +0100

    Refactored parser, implemented PR feedback

--- client/src/parser.js
+++ client/src/parser.js
@@ -1,98 +1,109 @@
-const { parse } = require('path');
+/**
+ * @description Receives an array of bmpnTasks and generates the .robot code for the tasks sections
+ * @returns {string} Generated .robot code for the tasks section
+ */
+function generateCodeForMultiple(bpmnTasks) {
+  let lastApplication = 'None';
+  let codeToAppend = "";
+  for (let task in bpmnTasks) {
+    let bpmnTaskProps;
+    bpmnTaskProps =
+      bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
+      'camunda:property'
+      ];
+    for (let propsIndex in bpmnTaskProps) {
+      if (propsIndex == 0) {
+        let currentApplication = bpmnTaskProps[0]._attributes.value
+        if (currentApplication !== lastApplication) {
+          codeToAppend += bpmnTasks[task]['_attributes']['name'] + '\n';
+          lastApplication = bpmnTaskProps[propsIndex]._attributes.value;
+        }
+      } else if (propsIndex > 0) {
+        codeToAppend += '  ' + bpmnTaskProps[propsIndex]._attributes.value;
+      }
+    }
+    codeToAppend += '\n';
+  }
+  return codeToAppend
+}
+
+/**
+ * @description Receives a single bmpnTask and generates the .robot code for the tasks sections
+ * @returns {string} Generated .robot code for the tasks section
+ */
+function generateCodeForSingle(bpmnTasks) {
+  let codeToAppend = "";
+  let taskExtensions = bpmnTasks['bpmn2:extensionElements'];
+
+  let bpmnTask = bpmnTasks;
+  codeToAppend += bpmnTask['_attributes']['name'] + '\n';
+  let bpmnTaskProps;
+  bpmnTaskProps = taskExtensions['camunda:properties']['camunda:property'];
+  for (let propsIndex in bpmnTaskProps) {
+    if (propsIndex > 0) {
+      codeToAppend += '  ' + bpmnTaskProps[propsIndex]._attributes.value;
+    }
+  }
+  return codeToAppend;
+}
+
+/**
+ * @description Receives all bpmnTasks and creates a list of all distinct applications needed
+ * @returns {array} List of applications as string
+ */
+function collectUsedApplications(bpmnTasks) {
+  let applications = []
+  for (let task in bpmnTasks) {
+    if (bpmnTasks[task]['bpmn2:extensionElements'] !== undefined) {
+      const rpaApp =
+        bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
+          'camunda:property'
+        ][0]._attributes.value;
+      if (!applications.includes(rpaApp)) {
+        applications.push(rpaApp);
+      }
+    }
+  }
+  return applications
+}
 
 /**
  * @description Parses an JSON created from the .bpmn xml of the model to an executable .robot file
  * @returns {string} Code that has to be put in .robot file
  */
-
-// this should be refactored to a dictionary
-function taskLib(app) {
-  if (app == 'Excel.Files') return 'RPA.Excel.Files';
-  if (app == 'Browser') return 'RPA.Browser';
-}
-
 function parseDiagramJson(json_data) {
   let parsedCode = '';
+  json_data = require("./parser/convertedModelForTesting1Activity.json")
   var bpmnTasks = json_data['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
 
-  // create Settings statements
   parsedCode += '*** Settings ***\n';
   parsedCode += 'Documentation  Our first parsed RPA\n';
 
   if (bpmnTasks !== undefined) {
-    console.log('bpmnTasks: ' + bpmnTasks);
     let codeToAppend = '';
-    if (typeof bpmnTasks[0] === 'object') {
-      // collect apps being used
-      const applications = [];
-      for (let task in bpmnTasks) {
-        const rpaApp =
-          bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
-            'camunda:property'
-          ][0]._attributes.value;
-        if (!applications.includes(rpaApp)) {
-          applications.push(rpaApp);
-        }
-      }
-      console.log(applications);
+    if (Array.isArray(bpmnTasks)) {
+      const applications = collectUsedApplications(bpmnTasks)
 
-      // import libraries needed for apps
       for (let app in applications) {
-        parsedCode += 'Library    ' + taskLib(applications[app]) + '\n';
+        parsedCode += 'Library    ' + "RPA." + applications[app] + '\n';
       }
 
       // idealy we use the keyword statement for each task, currently not working out of the box
       parsedCode += '\n*** Tasks ***\n';
-
-      // add code for each task
-      let currentApplication = 'None';
-      for (let task in bpmnTasks) {
-        let bpmnTaskProps;
-        bpmnTaskProps =
-          bpmnTasks[task]['bpmn2:extensionElements']['camunda:properties'][
-            'camunda:property'
-          ];
-        for (let i in bpmnTaskProps) {
-          if (i == 0) {
-            if (bpmnTaskProps[i]._attributes.value !== currentApplication) {
-              codeToAppend += bpmnTasks[task]['_attributes']['name'] + '\n';
-              currentApplication = bpmnTaskProps[i]._attributes.value;
-            }
-          } else if (i > 0) {
-            codeToAppend += '  ' + bpmnTaskProps[i]._attributes.value;
-          }
-        }
-        codeToAppend += '\n';
-      }
+      codeToAppend += generateCodeForMultiple(bpmnTasks)
     } else {
-      // collect app being used
-
       let taskExtensions = bpmnTasks['bpmn2:extensionElements'];
+
       var application =
         taskExtensions['camunda:properties']['camunda:property'][0]._attributes
           .value;
+      codeToAppend += 'Library    ' + "RPA." + application + '\n';
 
-      // import libraries needed for apps
-      parsedCode += 'Library    ' + taskLib(application) + '\n';
-
-      // idealy we use the keyword statement for each task, currently not working out of the box
-      parsedCode += '\n*** Tasks ***\n';
-
-      // add code for task
-
-      let bpmnTask = bpmnTasks;
-      codeToAppend += bpmnTask['_attributes']['name'] + '\n';
-      let bpmnTaskProps;
-      bpmnTaskProps = taskExtensions['camunda:properties']['camunda:property'];
-      for (let i in bpmnTaskProps) {
-        if (i > 0) {
-          codeToAppend += '  ' + bpmnTaskProps[i]._attributes.value;
-        }
-      }
+      codeToAppend += '\n*** Tasks ***\n';
+      codeToAppend += generateCodeForSingle(bpmnTasks)
     }
     parsedCode += codeToAppend;
   }
-  console.log(parsedCode);
   return parsedCode;
 }
 
