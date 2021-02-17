eee9f551aeef2fd96f55be9e1cfd460af3ed64ba
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-02-17 16:32:54 +0100

    fix functional component linter warnings

--- client/src/components/content/RobotContainer/CreateRobotContainer.jsx
+++ client/src/components/content/RobotContainer/CreateRobotContainer.jsx
@@ -6,28 +6,31 @@ import styles from './RobotContainer.module.css';
 
 const { Title } = Typography;
 
-const addRobot = () => alert("Adding Robots is currently not supported!");
-
 /**
  * @component
  * @description #todo
  * @example return #todo
  */
-const CreateRobotContainer = () => (
-    <Col xs={24} sm={12} md={8} xl={6}>
-        <Col className={styles.innerBoxCreateRobot} style={{ height: '13rem', padding: '1rem' }}>
-            <Row align="middle" style={{ height: '70%' }}>
-                <Col type="flex" span={24}>
-                    <PlusCircleOutlined onClick={addRobot} className={styles.clickiconCreateRobot} style={{ fontSize: '6rem' }} />
-                </Col>
-            </Row>
+const CreateRobotContainer = () => {
 
-            <Row justify="space-around" align="middle" style={{ height: '40%' }}>
-                <Title className={styles.title} level={3} >
-                    Create new Robot
+    const addRobot = () => alert("Adding Robots is currently not supported!");
+
+    return (
+        <Col xs={24} sm={12} md={8} xl={6}>
+            <Col className={styles.innerBoxCreateRobot} style={{ height: '13rem', padding: '1rem' }}>
+                <Row align="middle" style={{ height: '70%' }}>
+                    <Col type="flex" span={24}>
+                        <PlusCircleOutlined onClick={addRobot} className={styles.clickiconCreateRobot} style={{ fontSize: '6rem' }} />
+                    </Col>
+                </Row>
+
+                <Row justify="space-around" align="middle" style={{ height: '40%' }}>
+                    <Title className={styles.title} level={3} >
+                        Create new Robot
                 </Title>
-            </Row>
-        </Col>
-    </Col >
-);
+                </Row>
+            </Col>
+        </Col >
+    )
+};
 export default CreateRobotContainer;
--- client/src/components/content/RobotContainer/RobotContainer.jsx
+++ client/src/components/content/RobotContainer/RobotContainer.jsx
@@ -7,42 +7,45 @@ import styles from './RobotContainer.module.css';
 
 const { Title } = Typography;
 
-const startRobot = () => alert("Running the Robot is currently not supported!");
-const editRobot = () => alert("Editing the Robot is currently not supported!");
-
-const shortenTitle = (robotName) => {
-    const titleLength = 18;
-    if (robotName.length < titleLength) {
-        return robotName;
-    }
-    return `${robotName.substring(0, titleLength - 2)}...`;
-}
-
 /**
  * @component
  * @description #todo
  * @example return #todo
  */
-const RobotContainer = (props) => (
-    <Col xs={24} sm={12} md={8} xl={6}>
-        <Col className={styles.innerBox} style={{ height: '13rem', padding: '1rem' }}>
-            <Row align="middle" style={{ height: '55%' }}>
-                <Col type="flex" span={12}>
-                    <PlayCircleOutlined onClick={startRobot} className={styles.clickicon} style={{ fontSize: '4rem' }} />
-                </Col>
-                <Col type="flex" span={12}>
-                    <EditOutlined onClick={editRobot} className={styles.clickicon} style={{ fontSize: '4rem' }} />
-                </Col>
-            </Row>
+const RobotContainer = (props) => {
 
-            <Row justify="space-around" align="middle" style={{ height: '45%' }}>
-                <Title className={styles.title} level={3} editable >
-                    {shortenTitle(props.robotName)}
-                </Title>
-            </Row>
-        </Col>
-    </Col >
-);
+    const startRobot = () => alert("Running the Robot is currently not supported!");
+    const editRobot = () => alert("Editing the Robot is currently not supported!");
+
+    const shortenTitle = (robotName) => {
+        const titleLength = 18;
+        if (robotName.length < titleLength) {
+            return robotName;
+        }
+        return `${robotName.substring(0, titleLength - 2)}...`;
+    }
+
+    return (
+        <Col xs={24} sm={12} md={8} xl={6}>
+            <Col className={styles.innerBox} style={{ height: '13rem', padding: '1rem' }}>
+                <Row align="middle" style={{ height: '55%' }}>
+                    <Col type="flex" span={12}>
+                        <PlayCircleOutlined onClick={startRobot} className={styles.clickicon} style={{ fontSize: '4rem' }} />
+                    </Col>
+                    <Col type="flex" span={12}>
+                        <EditOutlined onClick={editRobot} className={styles.clickicon} style={{ fontSize: '4rem' }} />
+                    </Col>
+                </Row>
+
+                <Row justify="space-around" align="middle" style={{ height: '45%' }}>
+                    <Title className={styles.title} level={3} editable >
+                        {shortenTitle(props.robotName)}
+                    </Title>
+                </Row>
+            </Col>
+        </Col >
+    )
+};
 export default RobotContainer;
 
 RobotContainer.propTypes = {
