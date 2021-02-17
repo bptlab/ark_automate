bc200146a6e40fe5a469a67b7fe7448322395be4
Sandro Speh <sandro.speh@student.hpi.de>
2021-02-17 17:19:50 +0100

    refactored parser

--- server/services/BpmnToSsotParsing/BpmnToSsotParsing.jsx
+++ server/services/BpmnToSsotParsing/BpmnToSsotParsing.jsx
@@ -1,9 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 
 /**
  * @category Server
  * @module
  */
-
 
 /**
  * @description Creates a base element of the single source of truth
@@ -20,34 +20,42 @@ const createBaseElement = (id) => {
     return baseElement
 };
 
-
 /**
- * @description Parses an JSON created from the .bpmn xml of the model to the single source of truth
- * @returns {string} Code that has to be put in single source of truth file
+ * @description Creates the metadata object of the single source of truth
+ * @returns {object}  Metadata object of the single source of truth
  */
-const parseDiagramToSsot = (bpmnJson) => {
-
-    let ssotJson = '';
-    const ssot = {}
-
+const createMetadataObj = () => {
     const metadataObj = {
         "robotId": "exampleRobot",
         "starterId": "exampleID"
     }
-    ssot["robotMetadata"] = metadataObj
+    return metadataObj
+};
 
+/**
+ * @description Checks if the given id can be found in the given Array of element objects
+ * @returns {boolean}  Boolean if element is tracked in Array
+ */
+const isElementTracked = (elementsArray, id) => {
+    if (elementsArray.find(element => (element.id === id))) {
+        return true
+    }
+    return false
+}
 
+/**
+ * @description Creates the array full of elements by iterating over the
+ * referenced ids in the flow and adding new elements if they have not been added yet
+ * @returns {Array}  Array of elements with their id, successors and predecessors
+ */
+const findElements = (flows) => {
     const elementsArray = []
-    const flows = bpmnJson['bpmn2:definitions']['bpmn2:process']['bpmn2:sequenceFlow'];
-
-
 
     flows.forEach((flow) => {
         const source = flow._attributes.sourceRef
         const target = flow._attributes.targetRef
-        // check source of flow
-        if (!elementsArray.find(element => (element.id === source))) {
-            // not in elementsArray
+
+        if (!isElementTracked(elementsArray, source)) {
             const newElement = createBaseElement(source)
             newElement.successorIds.push(target)
             elementsArray.push(newElement)
@@ -56,9 +64,7 @@ const parseDiagramToSsot = (bpmnJson) => {
             sourceElement.successorIds.push(target)
         }
 
-        // check target of flow
-        if (!elementsArray.find(element => (element.id === target))) {
-            // not in elementsArray
+        if (!isElementTracked(elementsArray, target)) {
             const newElement = createBaseElement(target)
             newElement.predecessorIds.push(source)
             elementsArray.push(newElement)
@@ -68,46 +74,73 @@ const parseDiagramToSsot = (bpmnJson) => {
         }
     })
 
-    const bpmnActivities = bpmnJson['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
+    return elementsArray
+}
 
+/**
+ * @description Enriches elements in the elementsArray that should be of type instruction
+ * @returns {Array}  Array of elements for single source of truth
+ */
+const enrichInstructionElements = (elementsArray, bpmnActivities) => {
     bpmnActivities.forEach((activity) => {
-        const element = elementsArray.find(element => (element.id === activity._attributes.id))
-        element.name = activity._attributes.name
-        element.type = "INSTRUCTION"
-        if (activity["_attributes"]["arkRPA:application"]) {
-            // then is rpa activity
-            element["rpaApplication"] = activity["_attributes"]["arkRPA:application"]
-            element["rpaTask"] = activity["_attributes"]["arkRPA:task"]
+        const instructionElement = elementsArray.find(element => (element.id === activity._attributes.id))
+        instructionElement.name = activity._attributes.name
+        instructionElement.type = "INSTRUCTION"
+
+        if (activity._attributes["arkRPA:application"]) {
+            instructionElement.rpaApplication = activity._attributes["arkRPA:application"]
+            instructionElement.rpaTask = activity._attributes["arkRPA:task"]
 
             const parameterArray = []
-            const parameterObj = JSON.parse(activity["_attributes"]["arkRPA:inputVars"])
-            for (parameter in parameterObj) {
+            const parameterObj = JSON.parse(activity._attributes["arkRPA:inputVars"])
+            Object.keys(parameterObj).forEach((key) => {
                 const inputVar = {}
-                inputVar["name"] = parameter
-                inputVar["value"] = parameterObj[parameter]
-                inputVar["requireUserInput"] = true
+                inputVar.name = key
+                inputVar.value = parameterObj[key]
+                inputVar.requireUserInput = true
                 parameterArray.push(inputVar)
-            }
-            element["rpaParameters"] = parameterArray
-            // input element["rpaParameters"] = activity["_attributes"]["arkRPA:inputVars"]
+            })
+
+            instructionElement.rpaParameters = parameterArray
         }
-        element["outputVariable"] = ""
+        instructionElement.outputVariable = ""
 
     })
+    return elementsArray
+}
 
-    // detect events
-    const re = new RegExp("^Event_.*$")
+/**
+ * @description Enriches elements in the elementsArray that should be of type marker
+ * @returns {Array}  Array of elements for single source of truth
+ */
+const enrichMarkerElements = (elementsArray) => {
+    const eventRegularExpression = new RegExp("^Event_.*$")
     elementsArray.forEach(element => {
-        if (re.test(element.id)) {
+        if (eventRegularExpression.test(element.id)) {
             element.type = "MARKER"
         }
     })
+    return elementsArray
+}
 
-    ssot["elements"] = elementsArray
+/**
+ * @description Parses an JSON created from the .bpmn xml of the model to the single source of truth
+ * @returns {string} Code that has to be put in single source of truth file
+ */
+const parseDiagramToSsot = (bpmnJson) => {
 
-    ssotJson = JSON.stringify(ssot, null, 2)
-    // console.log(ssotJson)
-    return ssotJson;
+    const ssot = {}
+    ssot.robotMetadata = createMetadataObj()
+
+    const flows = bpmnJson['bpmn2:definitions']['bpmn2:process']['bpmn2:sequenceFlow'];
+    let elementsArray = findElements(flows)
+
+    const bpmnActivities = bpmnJson['bpmn2:definitions']['bpmn2:process']['bpmn2:task'];
+    elementsArray = enrichInstructionElements(elementsArray, bpmnActivities)
+    elementsArray = enrichMarkerElements(elementsArray)
+
+    ssot.elements = elementsArray
+    return JSON.stringify(ssot, null, 2);
 };
 
 module.exports = { parseDiagramToSsot }
