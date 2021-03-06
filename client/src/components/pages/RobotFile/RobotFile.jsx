caf89b3e7e51874e334eb3a472fcd98545a63b83
DanielWoelki <daniel.woelki@outlook.de>
2021-03-06 19:19:39 +0100

    PR small fixes implemented

--- client/src/components/pages/RobotFile/RobotFile.jsx
+++ client/src/components/pages/RobotFile/RobotFile.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
 import HeaderNavbar from '../../content/HeaderNavbar/HeaderNavbar';
-import { Typography } from 'antd';
 
 const { Title } = Typography;
 
