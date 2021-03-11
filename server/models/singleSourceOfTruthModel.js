9de8deb0c24c0adc2065239e9cb739f5c5a1595d
DanielWoelki <daniel.woelki@outlook.de>
2021-03-11 15:57:56 +0100

    updated ssot Model schema (mongoose)

--- server/models/singleSourceOfTruthModel.js
+++ server/models/singleSourceOfTruthModel.js
@@ -2,40 +2,35 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const robotMetadataSchema = new Schema({
-    robotId: String,
-    starterId: String,
-    robotName: String
-});
-
 const inputParameterSchema = new Schema({
-    name: String,
-    value: Schema.Types.Mixed,
-    requireUserInput: Boolean
+  name: String,
+  value: Schema.Types.Mixed,
+  requireUserInput: Boolean,
 });
 
 const instructionSchema = new Schema({
-    type: String,
-    name: String,
-    predecessorIds: [mongoose.Types.ObjectId],
-    successorIds: [mongoose.Types.ObjectId],
-    rpaApplication: String,
-    rpaTask: String,
-    rpaParameters: [inputParameterSchema],
-    outputVariable: Schema.Types.Mixed
+  type: String,
+  name: String,
+  predecessorIds: [mongoose.Types.ObjectId],
+  successorIds: [mongoose.Types.ObjectId],
+  rpaApplication: String,
+  rpaTask: String,
+  rpaParameters: [inputParameterSchema],
+  outputVariable: Schema.Types.Mixed,
 });
 
 const markerSchema = new Schema({
-    type: String,
-    name: String,
-    predecessorIds: [mongoose.Types.ObjectId],
-    successorIds: [mongoose.Types.ObjectId]
+  type: String,
+  name: String,
+  predecessorIds: [mongoose.Types.ObjectId],
+  successorIds: [mongoose.Types.ObjectId],
 });
 
 // eslint-disable-next-line camelcase
 const SSoT_Schema = new Schema({
-    robotMetadata: robotMetadataSchema,
-    elements: [instructionSchema, markerSchema ]
+  starterId: String,
+  robotName: String,
+  elements: [instructionSchema, markerSchema],
 });
 
 mongoose.model('SSoT', SSoT_Schema);
