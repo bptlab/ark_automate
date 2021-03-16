f1e154acb066e76cacca3fd74ae5efa44840a6a4
Sandro Speh <sandro.speh@student.hpi.de>
2021-03-16 11:16:44 +0100

    adapted container

--- client/src/components/content/RobotContainer/RobotContainer.jsx
+++ client/src/components/content/RobotContainer/RobotContainer.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Col, Row, Typography } from 'antd';
 import { PlayCircleOutlined, EditOutlined } from '@ant-design/icons';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './RobotContainer.module.css';
 import { changeSsotName } from '../../../api/ssotRetrieval';
@@ -17,23 +17,19 @@ const { Title } = Typography;
  * @category Client
  */
 const RobotContainer = (props) => {
-  const { robotId } = props;
-  const { userId } = props;
-  // eslint-disable-next-line react/destructuring-assignment
-  const [robotName, setRobotName] = useState(props.robotName);
+  const { robotId, robotName, userId } = props;
+  const [name, setRobotName] = useState(robotName);
 
-    const { robotId, robotName } = props;
-    const [name, setRobotName] = useState(robotName);
+  const renameRobot = (value) => {
+    changeSsotName(robotId, value)
+      .then(() => {
+        setRobotName(value);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    const renameRobot = (value) => {
-        changeSsotName(robotId, value)
-            .then(() => {
-                setRobotName(value);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-            
   /**
    * @description Will add a new job with the current robot id to the job list on the server side
    */
@@ -45,34 +41,41 @@ const RobotContainer = (props) => {
     }
   };
 
-    return (
-        <Col xs={24} sm={12} md={8} xl={6} xxl={4}>
-            <Col className={[styles.box, styles.robotBox]}>
-                <Row align="middle" style={{ height: '55%' }}>
-                    <Col type="flex" span={12}>
-                        <PlayCircleOutlined onClick={startRobot} className={styles.clickableIcon} />
-                    </Col>
-                    <Col type="flex" span={12}>
-                        <Link to={`/modeler/${robotId}`}>
-                            <EditOutlined className={styles.clickableIcon} />
-                        </Link>
-                    </Col>
-                </Row>
+  return (
+    <Col xs={24} sm={12} md={8} xl={6} xxl={4}>
+      <Col className={[styles.box, styles.robotBox]}>
+        <Row align='middle' style={{ height: '55%' }}>
+          <Col type='flex' span={12}>
+            <PlayCircleOutlined
+              onClick={startRobot}
+              className={styles.clickableIcon}
+            />
+          </Col>
+          <Col type='flex' span={12}>
+            <Link to={`/modeler/${robotId}`}>
+              <EditOutlined className={styles.clickableIcon} />
+            </Link>
+          </Col>
+        </Row>
 
-                <Row justify="space-around" align="middle" style={{ height: '45%' }}>
-                    <Title className={styles.title} level={3} editable={{ onChange: renameRobot }} >
-                        {name}
-                    </Title>
-                </Row>
-            </Col>
-        </Col >
-    )
+        <Row justify='space-around' align='middle' style={{ height: '45%' }}>
+          <Title
+            className={styles.title}
+            level={3}
+            editable={{ onChange: renameRobot }}
+          >
+            {name}
+          </Title>
+        </Row>
+      </Col>
+    </Col>
+  );
 };
 
 RobotContainer.propTypes = {
   robotName: PropTypes.string.isRequired,
   robotId: PropTypes.string.isRequired,
-  userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
 };
 
 export default RobotContainer;
