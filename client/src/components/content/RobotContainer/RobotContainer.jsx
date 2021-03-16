b2b058833e8ed55077775541bc18b2e94953e0c9
Sandro Speh <sandro.speh@student.hpi.de>
2021-03-16 10:18:59 +0100

    fixed merge conflicts

--- client/src/components/content/RobotContainer/RobotContainer.jsx
+++ client/src/components/content/RobotContainer/RobotContainer.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Col, Row, Typography } from 'antd';
 import { PlayCircleOutlined, EditOutlined } from '@ant-design/icons';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './RobotContainer.module.css';
 import { changeSsotName } from '../../../api/ssotRetrieval';
@@ -17,12 +17,8 @@ const { Title } = Typography;
  * @category Client
  */
 const RobotContainer = (props) => {
-  const { robotId } = props;
-  // eslint-disable-next-line react/destructuring-assignment
-  const [robotName, setRobotName] = useState(props.robotName);
-
-    const { robotId, robotName } = props;
-    const [name, setRobotName] = useState(robotName);
+  const { robotId, robotName } = props;
+  const [name, setRobotName] = useState(robotName);
 
   /**
    * @description Will add a new job with the current robot id to the job list on the server side
@@ -35,43 +31,50 @@ const RobotContainer = (props) => {
     }
   };
 
-    const renameRobot = (value) => {
-        changeSsotName(robotId, value)
-            .then(() => {
-                setRobotName(value);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+  const renameRobot = (value) => {
+    changeSsotName(robotId, value)
+      .then(() => {
+        setRobotName(value);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    return (
-        <Col xs={24} sm={12} md={8} xl={6} xxl={4}>
-            <Col className={[styles.box, styles.robotBox]}>
-                <Row align="middle" style={{ height: '55%' }}>
-                    <Col type="flex" span={12}>
-                        <PlayCircleOutlined onClick={startRobot} className={styles.clickableIcon} />
-                    </Col>
-                    <Col type="flex" span={12}>
-                        <Link to={`/modeler/${robotId}`}>
-                            <EditOutlined className={styles.clickableIcon} />
-                        </Link>
-                    </Col>
-                </Row>
+  return (
+    <Col xs={24} sm={12} md={8} xl={6} xxl={4}>
+      <Col className={[styles.box, styles.robotBox]}>
+        <Row align='middle' style={{ height: '55%' }}>
+          <Col type='flex' span={12}>
+            <PlayCircleOutlined
+              onClick={startRobot}
+              className={styles.clickableIcon}
+            />
+          </Col>
+          <Col type='flex' span={12}>
+            <Link to={`/modeler/${robotId}`}>
+              <EditOutlined className={styles.clickableIcon} />
+            </Link>
+          </Col>
+        </Row>
 
-                <Row justify="space-around" align="middle" style={{ height: '45%' }}>
-                    <Title className={styles.title} level={3} editable={{ onChange: renameRobot }} >
-                        {name}
-                    </Title>
-                </Row>
-            </Col>
-        </Col >
-    )
+        <Row justify='space-around' align='middle' style={{ height: '45%' }}>
+          <Title
+            className={styles.title}
+            level={3}
+            editable={{ onChange: renameRobot }}
+          >
+            {name}
+          </Title>
+        </Row>
+      </Col>
+    </Col>
+  );
 };
 
 RobotContainer.propTypes = {
-    robotName: PropTypes.string.isRequired,
-    robotId: PropTypes.string.isRequired,
+  robotName: PropTypes.string.isRequired,
+  robotId: PropTypes.string.isRequired,
 };
 
 export default RobotContainer;
--- client/src/utils/botExecution.jsx
+++ client/src/utils/botExecution.jsx
@@ -1,7 +1,7 @@
 // is a dummy function
 const isBotExecutable = () => {
   // eslint-disable-next-line no-constant-condition
-  if (true) {
+  if (false) {
     return true;
   }
   return false;
