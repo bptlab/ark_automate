c91629e189626b63c5b73443bec7eee5d8a3c70e
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-03-16 14:35:00 +0100

    merge ssot

--- client/src/api/ssotRetrieval.js
+++ client/src/api/ssotRetrieval.js
@@ -4,16 +4,32 @@
  */
 
 /**
+<<<<<<< HEAD
+ * @description Fetch all those SSOT names and Ids, which are available for the current user
+ * @param {String} userId - String including the user Id
+ */
+const fetchSSOTsForUser = async (userId) => {
+  const requestString = `/ssot/getAvailableBotsForUser/${userId}`;
+=======
  * @description Fetch all those Ssot names and Ids, which are available for the current user
  * @param {String} userId - String including the user Id
  */
 const fetchSsotsForUser = async (userId) => {
   const requestString = `/ssot/getAvailableRobotsForUser/${userId}`;
+>>>>>>> 79cc91dac611ee6001a59380269e4b03d928b199
   const response = await fetch(requestString);
   return response;
 };
 
 /**
+<<<<<<< HEAD
+ * @description Fetch all those SSOT names and Ids, which are available for the current user
+ * @param {String} userId - String including the user Id
+ */
+const changeSSOTName = async (SSOTid, newName) => {
+  const adjustedName = newName.replace(/\s/g, '+');
+  const requestString = `/ssot/renameBot?id=${SSOTid}&newName=${adjustedName}`;
+=======
  * @description This function renames the robot in Ssot
  * @param {String} ssotId - String including the ssotId
  * @param {String} newName - String with the new RobotName
@@ -42,13 +58,19 @@ const retrieveMetadataForBot = async (robotId) => {
 const createNewRobot = async (userid, newName) => {
   const adjustedName = newName.replace(/\s/g, '+');
   const requestString = `/ssot/createNewRobot?userid=${userid}&robotName=${adjustedName}`;
+>>>>>>> 79cc91dac611ee6001a59380269e4b03d928b199
   const response = await fetch(requestString);
   return response;
 };
 
 export {
+<<<<<<< HEAD
+  fetchSSOTsForUser,
+  changeSSOTName
+=======
   fetchSsotsForUser,
   changeSsotName,
   retrieveMetadataForBot,
   createNewRobot,
+>>>>>>> 79cc91dac611ee6001a59380269e4b03d928b199
 };
