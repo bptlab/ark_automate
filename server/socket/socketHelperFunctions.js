6a6d14f802dffe20b3b2c46b29d45c33d78b98f1
DanielWoelki <daniel.woelki@outlook.de>
2021-03-23 10:16:54 +0100

    update parameter documentation

--- server/socket/socketHelperFunctions.js
+++ server/socket/socketHelperFunctions.js
@@ -10,7 +10,7 @@ mongoose.set('useFindAndModify', false);
 
 /**
  * @description Fetches the ssot from the database and parses the ssot to robot code
- * @param {Integer} robotId the id of the robot we want the robot code for
+ * @param {String} robotId the id of the robot we want the robot code for
  */
 exports.getRobotCode = async (robotId) => {
   try {
@@ -28,7 +28,7 @@ exports.getRobotCode = async (robotId) => {
 
 /**
  * @description Fetches the ssot from the database and parses the ssot to robot code
- * @param {Integer} robotId the id of the robot we want the robot code for
+ * @param {String} robotId the id of the robot we want the robot code for
  */
 exports.getAllUserIds = async () => {
   try {
@@ -49,10 +49,10 @@ exports.getAllUserIds = async () => {
 
 /**
  * @description Creates a Job in the database for a robot execution of a specific user
- * @param {Integer} userId the id of the user that wants to execute a robot
- * @param {Integer} robotId the id of the robot to be executed
- * @param {Integer} status the current status of the job (either waiting, executing, success or failed)
- * @param {Integer} parameters different parameters the user defined before executing the robot
+ * @param {String} userId the id of the user that wants to execute a robot
+ * @param {String} robotId the id of the robot to be executed
+ * @param {String} status the current status of the job (either waiting, executing, success or failed)
+ * @param {Array} parameters different parameters the user defined before executing the robot
  */
 exports.createJob = async (userId, robotId, status, parameters) => {
   const job = new jobsModel.Job({
@@ -74,8 +74,8 @@ exports.createJob = async (userId, robotId, status, parameters) => {
 
 /**
  * @description Finds a specific job in the database and updates the status of the Job
- * @param {Integer} jobId the id of the job that we want to update
- * @param {Integer} status the current status of the job (either waiting, executing, success or failed)
+ * @param {String} jobId the id of the job that we want to update
+ * @param {String} status the current status of the job (either waiting, executing, success or failed)
  */
 exports.updateRobotJobStatus = (jobId, status) => {
   jobsModel.Job.findByIdAndUpdate(jobId, { status }, (err) => {
@@ -87,7 +87,7 @@ exports.updateRobotJobStatus = (jobId, status) => {
 
 /**
  * @description Finds all jobs with status waiting in the database for a specific user
- * @param {Integer} userId the id of the user we want all waiting jobs for
+ * @param {String} userId the id of the user we want all waiting jobs for
  */
 exports.getAllWaitingJobsForUser = async (userId) => {
   const jobList = await jobsModel.Job.find(
