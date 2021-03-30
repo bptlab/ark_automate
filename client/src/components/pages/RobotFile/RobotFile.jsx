273e3af1a236f5a294a1adc341b6ffe8920a1fc6
Kay Erik Jenss <kayerik.jenss@gmx.de>
2021-03-30 07:24:01 +0200

    initial version of code editor

--- client/src/components/pages/RobotFile/RobotFile.jsx
+++ client/src/components/pages/RobotFile/RobotFile.jsx
@@ -1,21 +1,70 @@
-import React from 'react';
-import { Layout, Typography } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Layout, Button, Space, Row, Col } from 'antd';
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
 import HeaderNavbar from '../../content/HeaderNavbar/HeaderNavbar';
-
-const { Title } = Typography;
+import 'prismjs/components/prism-robotframework';
+import getParsedRobotFile from "../../../api/ssot";
+import "prismjs/themes/prism.css";
 
 /**
  * @description View of the robot file
  * @category Client
  * @component
  */
-const RobotFile = () => (
-  <Layout>
-    <HeaderNavbar selectedKey={3} />
-    <Title style={{ paddingLeft: '2rem', paddingTop: '2rem' }}>
-      In future versions, the Robot Framework code can also be edited directly here.
-    </Title>
-  </Layout>
-);
+const RobotFile = () => {
+  const [code, setCode] = useState('Please wait, your robot file is being loaded.');
+  
+  /**
+   * @description Equivalent to ComponentDidMount in class based components
+   */
+  useEffect(() => {
+    const robotId = JSON.parse(sessionStorage.getItem('robotId'));
+    getParsedRobotFile(robotId)
+      .then((response) => response.text())
+      .then((robotCode) => {
+        setCode(robotCode);
+      });
+  }, []);
+
+  /**
+   * @description Gets called when the the button is pressed to save to the cloud.
+   * This function will retrieve the code from the editor, parse it to a ssot and write the 
+   * resulting ssot into the sessionStorage.
+   */
+  const onSaveToCloud = async () => {
+    const robotId = JSON.parse(sessionStorage.getItem('robotId'));
+    console.log(robotId);
+    console.log(code);
+    //please parse here to ssot and upsert
+  };
+
+  return (
+    <Layout >
+      <HeaderNavbar selectedKey={3} />
+        <Row justify="center" width='100%'>
+          <Col xs={24} sm={24} md={20} xl={16}>
+            <Space direction='vertical' size='middle' style={{ padding: '1rem', width: '100%' }}>
+              <Button type='primary' onClick={onSaveToCloud} style={{ width: '100%' }}>
+                Save changes to cloud
+              </Button>
+              <Editor
+                value={code}
+                onValueChange={(newCode) => setCode(newCode)}
+                highlight={highlightCode => highlight(highlightCode, languages.robotframework)}
+                padding={'1rem'}
+                style={{
+                  fontFamily: '"Fira code", "Fira Mono", monospace',
+                  fontSize: 20,
+                  'background-color': '#FFFFFF'
+                }}
+                tabsize={4}
+              />
+            </Space>
+          </Col>
+      </Row>
+    </Layout>
+  );
+}
 
 export default RobotFile;
