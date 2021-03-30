80f9c442074c0f6eabac3eade268886c1a0feea4
DanielWoelki <daniel.woelki@outlook.de>
2021-03-30 10:40:36 +0200

    create checkboxes and first function

--- client/src/components/content/ModelerSidebar/PropertiesPanel/PropertiesPanelSections/PPParameterSection.jsx
+++ client/src/components/content/ModelerSidebar/PropertiesPanel/PropertiesPanelSections/PPParameterSection.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import { Typography, Space } from 'antd';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Typography, Space, Checkbox } from 'antd';
 import styles from '../../ModelerSidebar.module.css';
 import PPParameterInput from '../PropertiesPanelComponents/PPParameterInput';
 
@@ -11,27 +11,47 @@ const { Text } = Typography;
  * @category Client
  * @component
  */
-const PPParameterSection = ({ onValueChange, variableList }) => (
+const PPParameterSection = ({ onValueChange, variableList }) => {
+  const [userInputRequired, setUserInputRequired] = useState(false);
+  const onChange = (e) => {
+    // also clear input field value and update variable in backend
+    if (e.target.checked) {
+      setUserInputRequired(true);
+    } else {
+      setUserInputRequired(false);
+    }
+  };
+  return (
     <>
-        <Text className={styles[`label-on-dark-background`]}>Parameter:</Text>
+      <Text className={styles[`label-on-dark-background`]}>Parameter:</Text>
 
-        <Space direction='vertical' style={{ width: '100%' }}>
-            {variableList.map((singleInput) => (
-                <PPParameterInput
-                    onValueChange={onValueChange}
-                    variableName={singleInput.name}
-                    isRequired={singleInput.isRequired}
-                    dataType={singleInput.type}
-                    value={singleInput.value}
-                />
-            ))}
-        </Space>
+      <Space direction='vertical' style={{ width: '100%' }}>
+        {variableList.map((singleInput) => (
+          <>
+            <PPParameterInput
+              onValueChange={onValueChange}
+              variableName={singleInput.name}
+              isRequired={singleInput.isRequired}
+              dataType={singleInput.type}
+              value={singleInput.value}
+              disabled={userInputRequired}
+            />
+            <Checkbox
+              onChange={onChange}
+              className={styles[`label-on-dark-background`]}
+            >
+              User Input required
+            </Checkbox>
+          </>
+        ))}
+      </Space>
     </>
-);
+  );
+};
 
 PPParameterSection.propTypes = {
-    onValueChange: PropTypes.func.isRequired,
-    variableList: PropTypes.arrayOf(PropTypes.shape).isRequired
+  onValueChange: PropTypes.func.isRequired,
+  variableList: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
 
 export default PPParameterSection;
