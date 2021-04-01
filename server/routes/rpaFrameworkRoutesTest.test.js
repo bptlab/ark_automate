d30aa07cee5fc2cb14cfc2ee8d1c1b8d4ce461d0
Sandro Speh <sandro.speh@student.hpi.de>
2021-04-01 10:22:44 +0200

    implemented rpa Task tests

--- server/routes/rpaFrameworkRoutesTest.test.js
+++ server/routes/rpaFrameworkRoutesTest.test.js
@@ -7,6 +7,8 @@ const dbLoader = require('../utils/TestingUtils/databaseLoader');
 const rpaController = require('../controllers/rpaFrameworkCommandsController');
 const testData = require('../utils/TestingUtils/testData');
 
+const ssotModel = require('../models/singleSourceOfTruthModel');
+
 /**
  * Connect to a new in-memory database before running any tests.
  */
@@ -65,5 +67,48 @@ describe('/commands/get-available-tasks-for-application', () => {
       testData.testRpaTask1.Task,
       testData.testRpaTask3.Task,
     ]);
+  });
+});
+
+describe('/commands/getAllParameters', () => {
+  it('retrieves the list of all available tasks correctly', async () => {
+    await dbLoader.loadTasksInDb();
+
+    const response = httpMocks.createResponse();
+    await rpaController.getAllParameters({}, response);
+    const data = await response._getData();
+
+    expect(response.statusCode).toBe(200);
+    expect(data.length).toBe(3);
+    expect(data[0].inputVars.length).not.toBe(0);
+    expect(data[0].inputVars.length).not.toBe(0);
+    expect(data[0].inputVars[0]).toHaveProperty(
+      'name',
+      testData.testRpaTask1.inputVars[0].name
+    );
+  });
+});
+
+describe('/commands/get-vars-for-task', () => {
+  it('retrieves the list of all available variables for a task', async () => {
+    await dbLoader.loadTasksInDb();
+
+    const request = httpMocks.createRequest({
+      query: {
+        application: testData.testRpaTask1.Application,
+        task: testData.testRpaTask1.Task,
+      },
+    });
+
+    const response = httpMocks.createResponse();
+    await rpaController.getVarsForTask(request, response);
+    const data = await response._getData();
+
+    expect(response.statusCode).toBe(200);
+    expect(data.inputVars.length).toBe(1);
+    expect(data.inputVars).toHaveProperty(
+      'name',
+      testData.testRpaTask1.inputVars.name
+    );
   });
 });
