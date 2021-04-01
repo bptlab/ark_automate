fca66c6d67aaf7108e17669bffb0870fb0f171a1
Sandro Speh <sandro.speh@student.hpi.de>
2021-04-01 16:17:11 +0200

    implemented botExecution

--- client/src/utils/botExecution.jsx
+++ client/src/utils/botExecution.jsx
@@ -1,9 +1,34 @@
-// is a dummy function
-const isBotExecutable = () => {
-  // eslint-disable-next-line no-constant-condition
-  if (false) {
-    return true;
-  }
-  return false;
+import { getParameterFromDB } from './attributeAndParamUtils';
+
+const retrieveRobotInformation = async (robotId) => getParameterFromDB(robotId);
+
+const checkForExecutability = (parameterObjects) => {
+  let executability = true;
+  parameterObjects.forEach((parameterObject) => {
+    parameterObject.rpaParameters.forEach((parameter) => {
+      if (parameter.isRequired === true && parameter.value === '') {
+        console.error(
+          'Required parameter not specified in paramter',
+          parameter
+        );
+        executability = false;
+      }
+      const requiredType = parameter.type.toLowerCase();
+      if (requiredType !== typeof parameter.value) {
+        console.error(
+          `Required parameter has not specified type. Should be ${requiredType}, but is ${typeof parameter.value}`,
+          parameter
+        );
+        executability = false;
+      }
+    });
+  });
+  return executability;
 };
-export default isBotExecutable;
+
+const isRobotExecutable = async (robotId) => {
+  const parametersForRobot = await retrieveRobotInformation(robotId);
+  return checkForExecutability(parametersForRobot);
+};
+
+export { isRobotExecutable, checkForExecutability };
