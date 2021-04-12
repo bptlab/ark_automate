fbfde72ead54f3d405101b66cbd06062b5d3fde0
Sandro Speh <sandro.speh@student.hpi.de>
2021-04-12 14:50:35 +0200

    working solution with weird behaviour implemented

--- client/src/utils/botExecution.jsx
+++ client/src/utils/botExecution.jsx
@@ -1,12 +1,18 @@
-import { getParameterFromDB } from './attributeAndParamUtils';
+import {
+  getParameterForRobotFromDB,
+  getAttributesFromDB,
+} from './attributeAndParamUtils';
 
-const retrieveRobotInformation = async (robotId) => getParameterFromDB(robotId);
-
-const checkParamsForExecutability = (parameterObjects) => {
+const configuredRobotParamsCorrectly = (parameterObjects) => {
   let executability = true;
-  parameterObjects.forEach((parameterObject) => {
-    parameterObject.rpaParameters.forEach((parameter) => {
-      if (parameter.isRequired === true && parameter.value === '') {
+  Array.prototype.forEach.call(parameterObjects, (parameterObject) => {
+    Array.prototype.forEach.call(parameterObject.rpaParameters, (parameter) => {
+      if (
+        parameter.isRequired === true &&
+        (parameter.requireUserInput === false ||
+          parameter.requireUserInput === undefined) &&
+        parameter.value === ''
+      ) {
         console.error(
           'Required parameter not specified in paramter',
           parameter
@@ -26,13 +32,55 @@ const checkParamsForExecutability = (parameterObjects) => {
   return executability;
 };
 
-const isRobotExecutable = (robotId) => {
-  const response = true;
-  retrieveRobotInformation(robotId).then((parametersForRobot) => {
-    checkParamsForExecutability(parametersForRobot);
-    // response = checkForExecutability(parametersForRobot);
+const configuredRobotActivitesCorrectly = (attributeObjects) => {
+  let executability = true;
+  Array.prototype.forEach.call(attributeObjects, (attributeObject) => {
+    if (attributeObject.rpaApplication === '') {
+      console.error(
+        `RPA Application of ${attributeObject.activityId} not specified`,
+        attributeObject
+      );
+      executability = false;
+    }
+    if (attributeObject.rpaTask === '') {
+      console.error(
+        `RPA Task of ${attributeObject.activityId} not specified`,
+        attributeObject
+      );
+      executability = false;
+    }
   });
-  return response;
+  return executability;
 };
 
-export { isRobotExecutable, checkParamsForExecutability };
+const isRobotExecutable = (robotId) => {
+  let areActivitiesCorrect;
+  let areParamsCorrect;
+
+  getAttributesFromDB(robotId)
+    .then((response) => response.json())
+    .then((data) => {
+      areActivitiesCorrect = configuredRobotActivitesCorrectly(data);
+    });
+
+  getParameterForRobotFromDB(robotId)
+    .then((response) => response.json())
+    .then((data) => {
+      areParamsCorrect = configuredRobotParamsCorrectly(data);
+    })
+    .then(() => {
+      if (areActivitiesCorrect && areParamsCorrect) {
+        return true;
+      }
+
+      console.log(areActivitiesCorrect, 'DAS LIEGT AN AKITIVTÄT');
+      console.log(areParamsCorrect, 'DAS LIEGT AN PARAMS');
+      return false;
+    });
+};
+
+export {
+  isRobotExecutable,
+  configuredRobotParamsCorrectly,
+  configuredRobotActivitesCorrectly,
+};
--- client/src/utils/botExecution.test.js
+++ client/src/utils/botExecution.test.js
@@ -1,4 +1,7 @@
-import { checkParamsForExecutability } from './botExecution';
+import {
+  configuredRobotParamsCorrectly,
+  configuredRobotActivitesCorrectly,
+} from './botExecution';
 
 const paramObject1 = {
   activityId: 'Activity_0ay88v7',
@@ -80,25 +83,109 @@ const paramObjectWithWrongType = {
   ],
 };
 
-describe('Checking Bot for Executability', () => {
+const paramObjectWithLaterSpecifiedUserInput = {
+  activityId: 'Activity_0mgbrvu',
+  ssotId: '60619b7b5d691786a44a6093',
+  rpaParameters: [
+    {
+      name: 'path',
+      type: 'String',
+      requireUserInput: true,
+      isRequired: true,
+      infoText:
+        'Path to save to. If not given, uses path given when opened or created.',
+      index: 0,
+      value: '',
+    },
+  ],
+};
+
+const attributeObject1 = {
+  activityId: 'Activity_1kwfds7',
+  ssotId: '60618ee17e82bbf2ca91cbb6',
+  rpaApplication: 'Excel.Application',
+  rpaTask: 'Quit Excel',
+};
+
+const attributeObject2 = {
+  activityId: 'Activity_0ay88v7',
+  ssotId: '60618ee17e82bbf2ca91cbb6',
+  rpaApplication: 'HTTP',
+  rpaTask: 'Get Request',
+};
+
+const attributeObjectWithEmptyTask = {
+  activityId: 'Activity_0ay88v7',
+  ssotId: '60618ee17e82bbf2ca91cbb6',
+  rpaApplication: 'HTTP',
+  rpaTask: '',
+};
+
+const attributeObjectWithEmptyApplication = {
+  activityId: 'Activity_0ay88v7',
+  ssotId: '60618ee17e82bbf2ca91cbb6',
+  rpaApplication: '',
+  rpaTask: 'Get Request',
+};
+
+describe('Checking Bot parameters for Executability', () => {
   it('correctly assigns bot as executable', async () => {
     const correctListOfParameters = [paramObject1, paramObject2];
-    const response = checkParamsForExecutability(correctListOfParameters);
+    const response = configuredRobotParamsCorrectly(correctListOfParameters);
     expect(response).toBe(true);
   });
 
-  it('correctly assigns bot as not executable when required parameter is empty', async () => {
+  it('correctly assigns bot as not executable when required parameter is empty', () => {
     const incorrectListOfParameters = [
       paramObject1,
       paramObjectWithoutRequiredValue,
     ];
-    const response = checkParamsForExecutability(incorrectListOfParameters);
+    const response = configuredRobotParamsCorrectly(incorrectListOfParameters);
     expect(response).toBe(false);
   });
 
-  it('correctly assigns bot as not executable when parameter is of wrong type', async () => {
+  it('correctly assigns bot as not executable when parameter is of wrong type', () => {
     const incorrectListOfParameters = [paramObject1, paramObjectWithWrongType];
-    const response = checkParamsForExecutability(incorrectListOfParameters);
+    const response = configuredRobotParamsCorrectly(incorrectListOfParameters);
+    expect(response).toBe(false);
+  });
+
+  it('correctly recognizes a bot as executable if required paramter is empty but is specified later by user input', () => {
+    const listOfParameters = [
+      paramObject1,
+      paramObjectWithLaterSpecifiedUserInput,
+    ];
+    const response = configuredRobotParamsCorrectly(listOfParameters);
+    expect(response).toBe(true);
+  });
+});
+
+describe('Checking Bot attributes for Executability', () => {
+  it('correctly assigns bot as executable', () => {
+    const correctListOfParameters = [attributeObject1, attributeObject2];
+    const response = configuredRobotActivitesCorrectly(correctListOfParameters);
+    expect(response).toBe(true);
+  });
+
+  it('correctly assigns bot as not executable when application is empty', () => {
+    const incorrectListOfParameters = [
+      attributeObject1,
+      attributeObjectWithEmptyApplication,
+    ];
+    const response = configuredRobotActivitesCorrectly(
+      incorrectListOfParameters
+    );
+    expect(response).toBe(false);
+  });
+
+  it('correctly assigns bot as not executable when task is empty', () => {
+    const incorrectListOfParameters = [
+      attributeObject1,
+      attributeObjectWithEmptyTask,
+    ];
+    const response = configuredRobotActivitesCorrectly(
+      incorrectListOfParameters
+    );
     expect(response).toBe(false);
   });
 });
