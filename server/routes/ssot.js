daa9a4fe0e350658a45a6fa125934c408974f6d0
WolfgangDaniel <42138601+WolfgangDaniel@users.noreply.github.com>
2021-04-21 15:53:01 +0200

    delete all unused api calls / routes that we won't need in the future

--- server/routes/ssot.js
+++ server/routes/ssot.js
@@ -9,7 +9,7 @@ const variableController = require('../controllers/ssotVariableController');
 const rpaAttributesController = require('../controllers/ssotRpaAttributes');
 
 /// PARSING ROUTES ///
-router.get('/parser/get-robot-code', parsingController.getRobotCode); // not used anymore
+router.get('/parser/get-robot-code', parsingController.getRobotCode); // needed in the future
 router.get('/parser/getForId/:robotId', parsingController.getRobotCodeForId);
 
 /// SSOT ROUTES ///
@@ -23,26 +23,23 @@ router.get(
   '/retrieveMetadataForRobot/:robotId',
   retrievalController.retrieveRobotMetadata
 );
-router.get('/shareRobotWithUser', retrievalController.shareRobotWithUser); // not used
+router.get('/shareRobotWithUser', retrievalController.shareRobotWithUser); // needed in the future
 router.get('/createNewRobot', retrievalController.createNewRobot);
 router.post('/overwriteRobot/:robotId', retrievalController.overwriteRobot);
 
 /// VARIABLE ROUTES ///
-router.get('/getVariablesForNewTask', variableController.getVariablesForNewTask); // not used
-router.get('/getVariables', variableController.getVariables); // not used
-router.get('/getVariables', variableController.getVariablesForTaskApp); // not used
-router.get('/checkForExistingVariables', variableController.checkForExistingVariables); // not used
-router.post('/updateInputAndOutput', variableController.updateVariables); // not used
-router.post('/updateInputParameter', variableController.updateOnlyInputParams); // not used
-router.post('/updateOutputVariableName', variableController.updateOnlyOutputVarName); // not used
 router.post('/updateManyParameters', variableController.updateMany);
-router.get('/getAllParameters/:robotId', variableController.retrieveParametersForRobot);
+router.get(
+  '/getAllParameters/:robotId',
+  variableController.retrieveParametersForRobot
+);
 router.get('/', variableController.retrieveParametersForRobot);
 
 /// RPA ATTRIBUTES ROUTES ///
-router.get('/getAttributes', rpaAttributesController.getAttributes); // not used
-router.post('/updateAttributes', rpaAttributesController.updateAttributes); // not used
 router.post('/updateManyAttributes', rpaAttributesController.updateMany);
-router.get('/getAllAttributes/:robotId', rpaAttributesController.retrieveAttributesForRobot);
+router.get(
+  '/getAllAttributes/:robotId',
+  rpaAttributesController.retrieveAttributesForRobot
+);
 
 module.exports = router;
