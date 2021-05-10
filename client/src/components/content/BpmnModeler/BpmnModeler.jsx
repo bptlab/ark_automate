61827607113debe0a79763e3b48772ab5b1a7303
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-05-10 14:55:11 +0200

    hide unsupported bpmn activites

--- client/src/components/content/BpmnModeler/BpmnModeler.jsx
+++ client/src/components/content/BpmnModeler/BpmnModeler.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import CamundaBpmnModeler from 'bpmn-js/lib/Modeler';
 import { Layout } from 'antd';
 import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
 import PropTypes from 'prop-types';
 import CliModule from 'bpmn-js-cli';
+import PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
 import { emptyBpmn } from '../../../resources/modeler/empty.bpmn';
 import styles from './BpmnModeler.module.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
@@ -20,6 +20,37 @@ const { Content } = Layout;
 const BpmnModeler = (props) => {
   let newModeler;
 
+  const { getPaletteEntries } = PaletteProvider.prototype;
+  PaletteProvider.prototype.getPaletteEntries = function () {
+    const entries = getPaletteEntries.apply(this);
+    delete entries['create.intermediate-event'];
+    delete entries['create.subprocess-expanded'];
+    delete entries['create.participant-expanded'];
+    delete entries['create.group'];
+    delete entries['create.exclusive-gateway'];
+    delete entries['create.data-store'];
+    delete entries['create.data-object'];
+    return entries;
+  };
+  class CustomContextPadProvider {
+    constructor(contextPad) {
+      contextPad.registerProvider(this);
+    }
+
+    getContextPadEntries(element) {
+      return function (entries) {
+        console.log(entries);
+        delete entries['append.text-annotation'];
+        delete entries['append.gateway'];
+        delete entries['append.intermediate-event'];
+        delete entries.replace;
+        return entries;
+      };
+    }
+  }
+
+  CustomContextPadProvider.$inject = ['contextPad'];
+
   /**
    * @description while the components were mounted, the BPMN-Modeler get's initialized
    */
@@ -29,14 +60,19 @@ const BpmnModeler = (props) => {
       keyboard: {
         bindTo: window,
       },
-      additionalModules: [propertiesProviderModule, CliModule],
-      moddleExtensions: {
-        camunda: camundaModdleDescriptor,
-      },
+      additionalModules: [
+        {
+          __init__: ['customContextPadProvider'],
+          customContextPadProvider: ['type', CustomContextPadProvider],
+        },
+        propertiesProviderModule,
+        CliModule,
+      ],
       cli: {
         bindTo: 'cli',
       },
     });
+
     props.onModelerUpdate(newModeler);
 
     const openBpmnDiagram = (xml) => {
