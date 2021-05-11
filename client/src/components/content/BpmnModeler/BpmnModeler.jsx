d2164f60f166a80664b186123d93ef63c8296019
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-05-11 10:08:29 +0200

    revert changes from #122

--- client/src/components/content/BpmnModeler/BpmnModeler.jsx
+++ client/src/components/content/BpmnModeler/BpmnModeler.jsx
@@ -4,7 +4,6 @@ import { Layout } from 'antd';
 import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
 import PropTypes from 'prop-types';
 import CliModule from 'bpmn-js-cli';
-import PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
 import { emptyBpmn } from '../../../resources/modeler/empty.bpmn';
 import styles from './BpmnModeler.module.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
@@ -20,37 +19,6 @@ const { Content } = Layout;
 const BpmnModeler = (props) => {
   let newModeler;
 
-  const { getPaletteEntries } = PaletteProvider.prototype;
-  PaletteProvider.prototype.getPaletteEntries = function () {
-    const entries = getPaletteEntries.apply(this);
-    delete entries['create.intermediate-event'];
-    delete entries['create.subprocess-expanded'];
-    delete entries['create.participant-expanded'];
-    delete entries['create.group'];
-    delete entries['create.exclusive-gateway'];
-    delete entries['create.data-store'];
-    delete entries['create.data-object'];
-    return entries;
-  };
-  class CustomContextPadProvider {
-    constructor(contextPad) {
-      contextPad.registerProvider(this);
-    }
-
-    getContextPadEntries(element) {
-      return function (entries) {
-        console.log(entries);
-        delete entries['append.text-annotation'];
-        delete entries['append.gateway'];
-        delete entries['append.intermediate-event'];
-        delete entries.replace;
-        return entries;
-      };
-    }
-  }
-
-  CustomContextPadProvider.$inject = ['contextPad'];
-
   /**
    * @description while the components were mounted, the BPMN-Modeler get's initialized
    */
@@ -60,14 +28,7 @@ const BpmnModeler = (props) => {
       keyboard: {
         bindTo: window,
       },
-      additionalModules: [
-        {
-          __init__: ['customContextPadProvider'],
-          customContextPadProvider: ['type', CustomContextPadProvider],
-        },
-        propertiesProviderModule,
-        CliModule,
-      ],
+      additionalModules: [propertiesProviderModule, CliModule],
       cli: {
         bindTo: 'cli',
       },
