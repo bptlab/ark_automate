dedfe3b811d60c2cb46756c188c746342f412390
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-05-11 10:25:19 +0200

    "fix" ESLINT Warnings

--- client/src/components/content/BpmnModeler/BpmnModeler.jsx
+++ client/src/components/content/BpmnModeler/BpmnModeler.jsx
@@ -21,6 +21,7 @@ const BpmnModeler = (props) => {
   let newModeler;
 
   const { getPaletteEntries } = PaletteProvider.prototype;
+  // eslint-disable-next-line func-names
   PaletteProvider.prototype.getPaletteEntries = function () {
     const entries = getPaletteEntries.apply(this);
     delete entries['create.intermediate-event'];
@@ -37,18 +38,20 @@ const BpmnModeler = (props) => {
       contextPad.registerProvider(this);
     }
 
-    getContextPadEntries(element) {
+    // eslint-disable-next-line class-methods-use-this
+    getContextPadEntries() {
+      // eslint-disable-next-line func-names
       return function (entries) {
-        console.log(entries);
-        delete entries['append.text-annotation'];
-        delete entries['append.gateway'];
-        delete entries['append.intermediate-event'];
-        delete entries['lane-insert-above'];
-        delete entries['lane-divide-two'];
-        delete entries['lane-divide-three'];
-        delete entries['lane-insert-below'];
-        delete entries.replace;
-        return entries;
+        const customizesEntries = entries;
+        delete customizesEntries['append.text-annotation'];
+        delete customizesEntries['append.gateway'];
+        delete customizesEntries['append.intermediate-event'];
+        delete customizesEntries['lane-insert-above'];
+        delete customizesEntries['lane-divide-two'];
+        delete customizesEntries['lane-divide-three'];
+        delete customizesEntries['lane-insert-below'];
+        delete customizesEntries.replace;
+        return customizesEntries;
       };
     }
   }
