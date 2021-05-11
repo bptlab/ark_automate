ff3d1309ee38a27f58648a92ab0b6c07cb385fcd
Lukas Hueller <lukas.hueller@student.hpi.uni-potsdam.de>
2021-05-11 16:58:48 +0200

    refactor method

--- client/src/components/content/BpmnModeler/BpmnModeler.jsx
+++ client/src/components/content/BpmnModeler/BpmnModeler.jsx
@@ -5,6 +5,7 @@ import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camu
 import PropTypes from 'prop-types';
 import CliModule from 'bpmn-js-cli';
 import PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
+import removeUnsupportedBpmnFunctions from './RemoveUnsupportedBpmnFunctions';
 import { emptyBpmn } from '../../../resources/modeler/empty.bpmn';
 import styles from './BpmnModeler.module.css';
 import 'bpmn-js/dist/assets/diagram-js.css';
@@ -20,44 +21,6 @@ const { Content } = Layout;
 const BpmnModeler = (props) => {
   let newModeler;
 
-  const { getPaletteEntries } = PaletteProvider.prototype;
-  // eslint-disable-next-line func-names
-  PaletteProvider.prototype.getPaletteEntries = function () {
-    const entries = getPaletteEntries.apply(this);
-    delete entries['create.intermediate-event'];
-    delete entries['create.subprocess-expanded'];
-    delete entries['create.participant-expanded'];
-    delete entries['create.group'];
-    delete entries['create.exclusive-gateway'];
-    delete entries['create.data-store'];
-    delete entries['create.data-object'];
-    return entries;
-  };
-  class CustomContextPadProvider {
-    constructor(contextPad) {
-      contextPad.registerProvider(this);
-    }
-
-    // eslint-disable-next-line class-methods-use-this
-    getContextPadEntries() {
-      // eslint-disable-next-line func-names
-      return function (entries) {
-        const customizesEntries = entries;
-        delete customizesEntries['append.text-annotation'];
-        delete customizesEntries['append.gateway'];
-        delete customizesEntries['append.intermediate-event'];
-        delete customizesEntries['lane-insert-above'];
-        delete customizesEntries['lane-divide-two'];
-        delete customizesEntries['lane-divide-three'];
-        delete customizesEntries['lane-insert-below'];
-        delete customizesEntries.replace;
-        return customizesEntries;
-      };
-    }
-  }
-
-  CustomContextPadProvider.$inject = ['contextPad'];
-
   /**
    * @description while the components were mounted, the BPMN-Modeler get's initialized
    */
@@ -67,7 +30,14 @@ const BpmnModeler = (props) => {
       keyboard: {
         bindTo: window,
       },
-      additionalModules: [propertiesProviderModule, CliModule],
+      additionalModules: [
+        {
+          __init__: ['customContextPadProvider'],
+          customContextPadProvider: ['type', removeUnsupportedBpmnFunctions()],
+        },
+        propertiesProviderModule,
+        CliModule,
+      ],
       cli: {
         bindTo: 'cli',
       },
