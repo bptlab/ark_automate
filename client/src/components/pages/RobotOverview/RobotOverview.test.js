4a455a8d750262d35e6e3a862f01fea06fffe186
Sandro Sp <38314662+SanJSp@users.noreply.github.com>
2021-05-17 16:02:17 +0200

    Revert "Refactored tests"

--- client/src/components/pages/RobotOverview/RobotOverview.test.js
+++ client/src/components/pages/RobotOverview/RobotOverview.test.js
@@ -56,7 +56,7 @@ beforeAll(() => jest.spyOn(window, 'fetch'));
 beforeEach(() => window.fetch.mockImplementation(mockFetch));
 
 describe('Testing functionality behind button to trigger function call for new but creation', () => {
-  it('checks if attempt to fetch occured twice', async () => {
+  it('check if attempt to fetch occured twice', async () => {
     act(() => {
       // eslint-disable-next-line react/jsx-filename-extension
       render(
